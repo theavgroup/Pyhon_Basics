21eec076457c473bdd264236d49089df9475a5b8
av1998 <42567487+av1998@users.noreply.github.com>
2019-03-05 11:56:20 +0530

    Auto Arrange Space

---
+++ 06.py
@@ -0,0 +1,18 @@
+favourite_language = 'Python '
+print(favourite_language)
+
+favourite_language = 'Python'
+print(favourite_language.rstrip())
+
+favourite_language = 'Python '
+favourite_language = favourite_language.rstrip()
+print(favourite_language)
+
+favourite_language = ' python '
+print(favourite_language.rstrip())
+
+favourite_language = ' python '
+print(favourite_language.lstrip())
+
+favourite_language = ' python '
+print(favourite_language.strip())
